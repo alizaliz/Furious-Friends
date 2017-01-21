63b98de69dec27610422378b116a7ec658916b59
Zain Ali <zain_ali8@hotmail.com>
2017-01-21 13:41:54 +1300

    Some basic ai functionality

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AIController : MonoBehaviour {
+
+	public Transform m_target;
+	public float m_followDistance;
+	public float m_pathDelay;
+
+	public float m_idleRange = 2;
+
+	[HideInInspector]
+	public state m_state;
+
+	private NavMeshAgent m_agent;
+	private Vector3 m_idleDest;
+	private Rigidbody m_Rigidbody; 
+	private float m_prevRestTime;
+
+	public enum state
+	{
+		chilling,
+		following
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+		m_state = state.chilling;
+		m_idleRange = Random.Range (0, m_idleRange);
+		m_idleDest = transform.forward * m_idleRange;
+		m_agent.SetDestination (m_Rigidbody.position  + m_idleDest);
+		m_prevRestTime = Time.time;
+
+	}
+
+	void Awake(){
+
+		m_agent = GetComponent<NavMeshAgent>();
+		m_Rigidbody = GetComponent<Rigidbody>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		switch(m_state){
+		case state.chilling:	
+			Chilling ();
+			break;
+		case state.following:
+			Following ();
+			break;
+		}
+	}
+
+	void Chilling ()
+	{
+		
+		if (!m_agent.pathPending && m_agent.pathStatus == NavMeshPathStatus.PathComplete && (Time.time - m_prevRestTime > m_pathDelay) )
+		{
+				m_idleDest *= -1; // Switch direction
+				m_agent.SetDestination (m_Rigidbody.position  + m_idleDest);
+
+				//agent.speed = Random.Range (1, gent.speed);
+
+				m_prevRestTime = Time.time; // update time
+
+		}
+
+	}
+
+	void Following (){
+		// Check -> Last path is complete
+		if ((  m_agent.pathStatus == NavMeshPathStatus.PathComplete ) && !m_agent.pathPending && (Time.time - m_prevRestTime > m_pathDelay) && (m_agent.destination != m_target.position)) {
+			m_agent.SetDestination (m_target.position);
+
+			m_prevRestTime = Time.time; // update time
+
+		}
+	}
+
+	//private Vector3 CalcTarget(){
+	//}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float m_Speed = 1;
+	public float m_TurnSpeed = 2;
+	private string m_MovementAxisName;     
+	private string m_TurnAxisName;         
+	private Rigidbody m_Rigidbody;  
+	private float m_MovementInputValue;    
+	private float m_TurnInputValue;    
+
+	// Use this for initialization
+	void Start () {
+		m_Rigidbody = GetComponent<Rigidbody>();
+		m_MovementInputValue = 0f;
+		m_MovementAxisName = "Vertical";
+		m_TurnAxisName = "Horizontal";
+		m_MovementInputValue = 0f;
+		m_TurnInputValue = 0f;
+	}
+	
+	// Update is called once per frame
+	private void Update()
+	{
+		// Store the player's input and make sure the audio for the engine is playing.
+		m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+		m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+
+	}
+
+	void FixedUpdate () {
+		// Move and turn the tank.
+		Move();
+		Turn ();
+
+
+	}
+
+	private void Move()
+	{
+		// Adjust the position of the tank based on the player's input.
+
+		Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+		m_Rigidbody.MovePosition (m_Rigidbody.position + movement);
+	}
+
+
+	private void Turn()
+	{
+		// Adjust the rotation of the tank based on the player's input.
+
+		float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+		Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+
+		m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
+
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Friend"))
+		{
+			AIController ai = other.GetComponent<AIController>();
+			ai.m_target = m_Rigidbody.transform;
+			ai.m_state = AIController.state.following;
+		}
+	}
+}
