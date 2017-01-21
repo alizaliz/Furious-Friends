55bcacb232bf6e7758a9a86b7f2f2c31e1749a1b
Event <event@mds.co.nz>
2017-01-21 20:29:09 +1300

    I see you

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    [Header("Cone Raycasting")]
+    public int m_ConeRays;
+    public float m_ConeAngle;
+
 	public float m_Speed = 1;
-	public float m_TurnSpeed = 2;
+	public float m_TurnSpeed =  2;
 	private string m_MovementAxisName;     
 	private string m_TurnAxisName;         
 	private Rigidbody m_Rigidbody;  
 	private float m_MovementInputValue;    
-	private float m_TurnInputValue;    
+	private float m_TurnInputValue;
+       
+    // Tag on Object to check for raycast collision, can change this to a more descriptive string
+    public string friends = "Friend";
+    public bool checkAllTags = false;
+    public Transform playerPosition;
+    // Wave range from player to AI
+    public float raycastDist = 500f;
 
 	// Use this for initialization
 	void Start () {
@@ -22,14 +33,73 @@ public class PlayerController : MonoBehaviour {
 		m_TurnInputValue = 0f;
 	}
 	
-	// Update is called once per frame
-	private void Update()
-	{
-		// Store the player's input and make sure the audio for the engine is playing.
-		m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
-		m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+    private void Wave()
+    {
+        // Check for raycast hit
+        HashSet<RaycastHit> coneHits = new HashSet<RaycastHit>();
+        //Ray ray = new Ray (transform.position, transform.forward);
 
-	}
+        float shortestDist;
+        shortestDist = Mathf.Infinity;
+
+        float forwardAngle = Mathf.Atan2(transform.forward.z, transform.forward.x);
+        forwardAngle -= Mathf.Deg2Rad * 90.0f;
+        float startAngleOffset = (Mathf.Deg2Rad * m_ConeAngle) / 2.0f;
+        for (int i = 0; i < m_ConeRays; i++)
+        {
+            float newAngle = forwardAngle + startAngleOffset + (i * (Mathf.Deg2Rad * m_ConeAngle / m_ConeRays));
+            Vector3 rayDir = new Vector3(Mathf.Cos(newAngle), 0.0f, Mathf.Sin(newAngle));
+            RaycastHit[] hits = Physics.RaycastAll(transform.position, rayDir, 10.0f);
+            Debug.DrawRay(transform.position, rayDir * 10.0f, Color.cyan);
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.transform.CompareTag("Friend"))
+                {
+                    coneHits.Add(hit);
+                }
+            }
+        }
+
+        foreach (RaycastHit hit in coneHits)
+        {
+            Debug.DrawRay(hit.transform.position, hit.transform.forward * 10.0f, Color.red);
+            Debug.Log("Hit: " + hit.collider.gameObject.name);
+
+            float angle = Vector3.Dot(hit.transform.forward, transform.position - hit.transform.position);
+            if (angle > 0.9f)
+            {
+                Debug.DrawRay(hit.transform.position, Vector3.up * 4.0f, Color.yellow);
+            }
+            else
+            {
+                Debug.DrawRay(hit.transform.position, Vector3.up * 4.0f, Color.red);
+            }
+        }
+    }
+
+	// Update is called once per frame
+	private void Update ()
+	{
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Wave();
+        }
+        //Debug.Log(hits);
+
+        //for (int i = 0; i < hits.Length; i++)
+        //{
+        //    if (hits[i].transform.tag == friends &&((hits[i].distance) < shortestDist))
+        //    {
+        //        shortestDist = Vector3.Distance(transform.position, hits[i].point);
+        //    }
+        //}
+
+
+        // Store the player's input and make sure the audio for the engine is playing.
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+
+    }
 
 	void FixedUpdate () {
 		// Move and turn the tank.
