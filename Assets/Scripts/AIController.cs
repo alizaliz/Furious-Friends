7b811043e6a8b7c1c6ed2abb8eada56b68258465
JeffMmc <jeffmc196@gmail.com>
2017-01-22 16:38:56 +1300

    Minor merge conflict

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -23,11 +23,7 @@ public class AIController : MonoBehaviour {
 	private float m_idleTurn;
 	private Rigidbody m_Rigidbody; 
 	private float m_prevRestTime;
-<<<<<<< HEAD
-=======
 	//private bool m_waved;
->>>>>>> bb58c101af50f31f76f120f2089897bab6b06937
-
 
     public AudioClip[] m_VoiceClips;
     public AudioClip m_Voice;
